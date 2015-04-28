b8cc97ad458949aca27de8e6538499fe9d91b23c
schnatterer <johannes@schnatterer.info>
2015-04-28 22:05:30 +0200

    Added license headers to all files and automated the process

--- src/main/java/info/schnatterer/itunes4j/entity/Track.java
+++ src/main/java/info/schnatterer/itunes4j/entity/Track.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (C) 2015 Johannes Schnatterer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package info.schnatterer.itunes4j.entity;
+
+import info.schnatterer.itunes4j.exception.ITunesException;
 
 import java.util.Date;
 
+import com4j.ComException;
 import com4j.itunes.IITFileOrCDTrack;
 
 public class Track {
@@ -16,8 +34,13 @@ public class Track {
 		return wrappedTrack.playedCount();
 	}
 
-	public void setPlayedCount(int playedCount) {
-		wrappedTrack.playedCount(playedCount);
+	public void setPlayedCount(int playedCount) throws ITunesException {
+		try {
+			// TODO do this for all methods that acces wrapped track
+			wrappedTrack.playedCount(playedCount);
+		} catch (ComException e) {
+			throw ITunesException.createITunesException(e);
+		}
 	}
 
 	public Date getPlayedDate() {
@@ -74,4 +97,9 @@ public class Track {
 	public void dispose() {
 		wrappedTrack.dispose();
 	}
+
+	protected IITFileOrCDTrack getWrappedTrack() {
+		return wrappedTrack;
+	}
+
 }
